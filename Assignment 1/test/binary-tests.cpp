217011ffc503bfa1d3008879226e8495bab28e5c
MarkNooneTCD <manoone@tcd.ie>
2017-10-10 15:04:49 +0100

    Wrote basic tests in order to get Catch and Cmake working

--- Assignment 1/test/binary-tests.cpp
+++ Assignment 1/test/binary-tests.cpp
@@ -5,15 +5,23 @@
 
 TEST_CASE( "Checking LCA Data Type", "[lca-tests]" ){
     
-    // Initialise data types here.
-    int one = 1;
+    // Let us create a basic Binary Tree.
+    Node * root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    root->right->left = newNode(6);
+    root->right->right = newNode(7);
 
     //Test outcomes here by dividing tests into sections and 
     // then manipulating the data type and check with REQUIRE.
-    SECTION( "resizing bigger changes size and capacity" ) {
-        v.resize( 10 );
+    SECTION( "Testing basic LCA answers." ) {
         
-        REQUIRE( v.size() == 10 );
-        REQUIRE( v.capacity() >= 10 );
+        REQUIRE( findLCA(root, 4, 5) ==  2)
+        REQUIRE( findLCA(root, 4, 6) ==  1)
+        REQUIRE( findLCA(root, 3, 4) ==  1)
+        REQUIRE( findLCA(root, 2, 4)) ==  2)
+    
     }
 }
