8d9bfd1998c84d913ad34b60c0402c331aafa432
MarkNooneTCD <manoone@tcd.ie>
2017-11-16 22:45:53 +0000

    Added the adjacency list class that keeps track of the connected nodes within a graph. Also updated the graph's header file

---
+++ Assignment 1-2/src/AdjacencyList.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include <list>
+#include <stack>
+#include <limits.h>
+#define INF INT_MAX
+
+class AdjListNode
+{
+        int v;
+        int weight;
+    public:
+        AdjListNode(int _v, int _w)
+        {
+            v = _v;
+            weight = _w;
+        }
+        int getV()
+        {
+            return v;
+        }
+        int getWeight()
+        {
+            return weight;
+        }
+};
