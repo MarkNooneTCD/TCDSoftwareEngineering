2e6cbb08aaac7c5db5f37de019b9bed69ccefff3
MarkNooneTCD <manoone@tcd.ie>
2017-11-17 15:56:23 +0000

    Updated binary tests to reflect new way to create a graph. Also created a testing DAG with cycles in order to test the latest LCA function.

--- Assignment 1-2/test/binary-tests.cpp
+++ Assignment 1-2/test/binary-tests.cpp
@@ -9,14 +9,16 @@ TEST_CASE( "Checking Find Path Function", "[path]" ){
 
     vector<int> path1;
 
-    // Let us create a basic Binary Tree.
-    Node * root = newNode(1);
-    root->left = newNode(2);
-    root->right = newNode(3);
-    root->left->left = newNode(4);
-    root->left->right = newNode(5);
-    root->right->left = newNode(6);
-    root->right->right = newNode(7);
+    int nodes = 7;
+    Graph g = new Graph(nodes);
+
+    // Add Edges
+    g.addEdge(0, 1, 1);
+    g.addEdge(0, 2, 1);
+    g.addEdge(1, 3, 1);
+    g.addEdge(1, 4, 1);
+    g.addEdge(2, 5, 1);
+    g.addEdge(2, 6, 1);
 
     //Test outcomes here by dividing tests into sections and
     // then manipulating the data type and check with REQUIRE.
@@ -50,14 +52,16 @@ TEST_CASE( "Checking Find Path Function", "[path]" ){
 
 TEST_CASE( "Checking LCA Function", "[lca]" ){
 
-    // Let us create a basic Binary Tree.
-    Node * root = newNode(1);
-    root->left = newNode(2);
-    root->right = newNode(3);
-    root->left->left = newNode(4);
-    root->left->right = newNode(5);
-    root->right->left = newNode(6);
-    root->right->right = newNode(7);
+    int nodes = 7;
+    Graph g = new Graph(nodes);
+
+    // Add Edges
+    g.addEdge(0, 1, 1);
+    g.addEdge(0, 2, 1);
+    g.addEdge(1, 3, 1);
+    g.addEdge(1, 4, 1);
+    g.addEdge(2, 5, 1);
+    g.addEdge(2, 6, 1);
 
     //Test outcomes here by dividing tests into sections and
     // then manipulating the data type and check with REQUIRE.
@@ -93,20 +97,52 @@ TEST_CASE( "Checking LCA Function", "[lca]" ){
   }
 
     TEST_CASE( "Checking LCA Function For DAG", "[lca-dag]" ){
+        /*
+          Graph Diagram:
+
+          +------+0+-----+
+          |       +      |
+          |       |      |
+          v       v      v
+          1       2      3
+          +--+    +      +
+          |  |    |      |
+          v  |    v      v
+          4  +--> 5      6
+          |       |   +-------+
+          v       v       v       v
+          7       8       9      10
+          +------+       +---+---+
+            v               v
+            11              12
+            +------+--------+
+                   v
+                  13
+
+        */
 
         // Let us create a Direct Acyclic Graph
         int nodes = 14;
         Graph g = new Graph(nodes);
 
-        // Create the vertex's
-        int const edges[14][3] = {
-          {0, 1, 1}, {0, 2, 1}, {0, 3, 1}, {1, 4, 1}, {1, 5, 1},
-          {2, 5, 1}, {3, 6, 1}, {4, 7, 1}, {5, 8, 1}, {7, 11, 1},
-          {8, 11, 1}, {9, 12, 1}, {10, 12, 1}, {12, 13, 1}
-        };
-
         // Add Edges
-        g.addEdges(edges);
+        g.addEdge(0, 1, 1);
+        g.addEdge(0, 2, 1);
+        g.addEdge(0, 3, 1);
+        g.addEdge(1, 4, 1);
+        g.addEdge(1, 5, 1);
+        g.addEdge(2, 5, 1);
+        g.addEdge(3, 6, 1);
+        g.addEdge(4, 7, 1);
+        g.addEdge(5, 8, 1);
+        g.addEdge(6, 9, 1);
+        g.addEdge(6, 10, 1);
+        g.addEdge(7, 11, 1);
+        g.addEdge(8, 11, 1);
+        g.addEdge(9, 12, 1);
+        g.addEdge(10, 12, 1);
+        g.addEdge(12, 13, 1);
+        g.addEdge(11, 13, 1);
 
         //Get Root Node
         DAGNode root = g.findNodeWithKey(0);
