52cf4fc995613d7959322e05ec1cc23d6fb0e4fe
MarkNooneTCD <manoone@tcd.ie>
2017-11-19 16:52:11 +0000

    Move the adjacency list to a header and cleaned it up

--- Assignment 1-2/include/AdjacencyList.hpp
+++ Assignment 1-2/include/AdjacencyList.hpp
@@ -1,25 +1,22 @@
 #include <iostream>
-#include <list>
-#include <stack>
-#include <limits.h>
-
-#include "../include/graph.hpp"
-#define INF INT_MAX
 
 class AdjListNode
 {
         int v;
         int weight;
+
     public:
         AdjListNode(int _v, int _w)
         {
             v = _v;
             weight = _w;
         }
+
         int getV()
         {
             return v;
         }
+
         int getWeight()
         {
             return weight;
