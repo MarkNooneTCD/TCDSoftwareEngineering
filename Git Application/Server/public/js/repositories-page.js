2f353ffcf34a1314cfc42ba6187096653ba22c00
MarkNooneTCD <manoone@tcd.ie>
2017-12-14 12:34:51 +0000

    Added requests to repository pages

--- Git Application/Server/public/js/repositories-page.js
+++ Git Application/Server/public/js/repositories-page.js
@@ -1,3 +1,67 @@
 $(document).ready(function() {
-  $("#profile-tag").click(function() {alert("Hello, repos!");});
+  showLoader();
+  initApp();
 });
+
+function initApp() {
+  var url = "/api/authorized/repos/"+getCookie("accessToken");
+
+  var request = $.get( url, function(res) {
+    var dataJsonObj = JSON.parse(res);
+    insertData(dataJsonObj);
+    showApp();
+  }).then();
+
+}
+
+function insertData(jsonRepoArray){
+  var i = 0;
+  var dataInjectionHtml = "";
+
+  <!-- Line -->
+
+
+  while(i < jsonRepoArray.length){
+    dataInjectionHtml += '<div class="row"><div class="col-lg-12"><hr /></div></div>' +
+                      '<div class="row" style="margin-top: 1vh; margin-bottom: 1vh;"><div class="col-lg-3 repository-info-heading"><h6>'+
+                      jsonRepoArray[i].name +
+                      '</h6></div><div class="col-lg-2 "><h6><span class="repo-label">ID:</span>'+
+                      jsonRepoArray[i].id +
+                      '</h6></div><div class="col-lg-6"><h6><span class="repo-label">Last Updated:</span>'+
+                      jsonRepoArray[i].updated_at +
+                      '</h6></div></div></div>';
+    i++;
+  }
+  // debugger;
+  document.getElementById("data-injection").innerHTML = dataInjectionHtml;
+}
+
+
+
+function showApp(){
+  document.getElementById("loading").style.display = 'none';
+  document.body.style.backgroundColor = "white";
+  document.getElementById("app").style.display = 'block';
+}
+function showLoader(){
+  document.getElementById("app").style.display = 'none';
+  document.body.style.backgroundColor = "#2f353e";
+  document.getElementById("loading").style.display = 'block';
+}
+
+function getCookie(name) {
+  var getCookieValues = function(cookie) {
+		var cookieArray = cookie.split('=');
+		return cookieArray[1].trim();
+	};
+
+	var getCookieNames = function(cookie) {
+		var cookieArray = cookie.split('=');
+		return cookieArray[0].trim();
+	};
+
+	var cookies = document.cookie.split(';');
+	var cookieValue = cookies.map(getCookieValues)[cookies.map(getCookieNames).indexOf(name)];
+	return (cookieValue === undefined) ? null : cookieValue;
+
+}
