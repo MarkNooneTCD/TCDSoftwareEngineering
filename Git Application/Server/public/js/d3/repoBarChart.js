f82529c79745e5e0d2722782462f2b882cae561f
MarkNooneTCD <manoone@tcd.ie>
2017-12-15 18:58:16 +0000

    forgot to save from last commit

--- Git Application/Server/public/js/d3/repoBarChart.js
+++ Git Application/Server/public/js/d3/repoBarChart.js
@@ -3,37 +3,7 @@ $(document).ready(function() {
   var parameters = location.search.substring(1).split("&");
   var temp = parameters[0].split("=");
   var l = unescape(temp[1]);
-  // console.log(l);
 
-  // Dummy data for testing the bar chart.
-  // var data = [
-  //   {letter: "A", frequency: .08167},
-  //   {letter: "B", frequency: .01492},
-  //   {letter: "C", frequency: .02780},
-  //   {letter: "D", frequency: .04253},
-  //   {letter: "E", frequency: .12702},
-  //   {letter: "F", frequency: .02288},
-  //   {letter: "G", frequency: .02022},
-  //   {letter: "H", frequency: .06094},
-  //   {letter: "I", frequency: .06973},
-  //   {letter: "J", frequency: .00153},
-  //   {letter: "K", frequency: .00747},
-  //   {letter: "L", frequency: .04025},
-  //   {letter: "M", frequency: .02517},
-  //   {letter: "N", frequency: .0},
-  //   {letter: "O", frequency: .07507},
-  //   {letter: "P", frequency: .01929},
-  //   {letter: "Q", frequency: .00098},
-  //   {letter: "R", frequency: .05987},
-  //   {letter: "S", frequency: .06333},
-  //   {letter: "T", frequency: .09056},
-  //   {letter: "U", frequency: .02758},
-  //   {letter: "V", frequency: .01037},
-  //   {letter: "W", frequency: .02465},
-  //   {letter: "X", frequency: .00150},
-  //   {letter: "Y", frequency: .01971},
-  //   {letter: "Z", frequency: .00074}
-  // ];
   var url = 'http://localhost:3000/api/authorized/repo/' + getCookie("username") +
             '/' + l +'/contributors/' + getCookie("accessToken");
 
